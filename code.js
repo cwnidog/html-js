3445600735aadbf29098e24ef10bc64f0e5d2e1b
jbbrokaw <jbrokaw@gmail.com>
2014-08-08 12:23:47 -0700

    Implements a delay in the loop

--- code.js
+++ code.js
@@ -1,44 +1,48 @@
-var p1div = document.getElementById("pic1");
-var p2div = document.getElementById("pic2");
+//Moves the tortoise and the hare on the page, based on previous assignment
 
-p1div.style.position = "relative";
-p2div.style.position = "relative";
-
-
-function Animal(name, speed, focus) {
-  this.name = name;
-  this.speed= speed; //distance moved per step
-  this.focus= focus; //scale of 0 to 10, probability of moving in a given step
+function Animal(speed, focus, imageID) {
+  this.speed = speed; //distance moved per step
+  this.focus = focus; //scale of 0 to 10, probability of moving in a given step
+  this.imageElement = document.getElementById(imageID);
+  this.imageElement.style.position = "relative";
   this.position = 0;
-  this.move = function() { //Makes a move
-    if (Math.random()*10 < this.focus) {//for focus = 8, moves 80% of the time
-      this.position+=speed;
+  this.move = function () { //Makes a move
+    if (Math.random() * 10 < this.focus) {//for focus = 8, moves 80% of the time
+      this.position += speed;
     }
-  }
-}
-
-
-function Race(animal1,animal2) {
-  this.animal1 = animal1;
-  this.animal2 = animal2;
-  this.totalDistance = 90;
-
-  this.run = function () {
-    do { //when somebody wins, we return, which kills the loop.
-      this.animal1.move();
-      this.animal2.move();
-      p1div.style.left = String(this.animal1.position) + "%";
-      p2div.style.left = String(this.animal2.position) + "%";
-    }
-    while ((animal1.position<this.totalDistance)&&
-          (animal2.position<this.totalDistance));
   };
 }
 
-var rabbit = new Animal("Peter",3,3); //relative speed is just the product, 9
-var turtle = new Animal("Beth", 1, 10);//rel. speed is 10, should always win
-                                      //(except sometimes short races)
+function Race(animal1, animal2) {
+  this.animal1 = animal1;
+  this.animal2 = animal2;
+  this.totalDistance = 90;
+  this.delay = 150;
+
+  this.step = function () {
+    this.animal1.move();
+    this.animal2.move();
+    this.animal1.imageElement.style.left = String(this.animal1.position) + "%";
+    this.animal2.imageElement.style.left = String(this.animal2.position) + "%";
+  };
+
+  this.reset = function () {
+    this.animal1.position = 0;
+    this.animal2.position = 0;
+    this.animal1.imageElement.style.left = String(this.animal1.position) + "%";
+    this.animal2.imageElement.style.left = String(this.animal2.position) + "%";
+  };
+}
+
+function runRace(race) {
+  race.step();
+  if ((race.animal1.position < race.totalDistance) &&
+        (race.animal2.position < race.totalDistance)) {
+    setTimeout(function () {runRace(race); }, race.delay);
+  }
+}
+
+var turtle = new Animal(1, 10, "pic1");
+var rabbit = new Animal(3, 3, "pic2");
 
 var bigRace = new Race(rabbit, turtle);
-
-
